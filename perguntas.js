5a20d04b5fd990ea4857d0595a00af039aa1cc8b
hihiKKKka <mhlk8386@gmail.com>
2024-12-04 11:34:15 -0300

    versão 1

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    "Hello Kitty", 
+    "Qual o nome completo da Hello Kitty?",  
+    "Kitty White é o nome completo da Hello Kitty" 
+)
+
+criaCartao(
+    "Hello Kitty",
+    "A Hello Kitty tem irmãos?",
+    "Sim, ela tem uma irmã gêmea chamada Mimmy White"
+)
+
+criaCartao(
+    "Hello Kitty",
+    "Quando foi criada a Sanrio?",
+    "Ela foi criada em 10 de agosto de 1960, por Yamanashi, Japão"
+)
+
+criaCartao(
+    "Hello Kitty",
+    'Qual é a cor favorita do Keroppi?',
+    "Sua cor favorita é preto"
+)
+
+criaCartao(
+    "Hello Kitty",
+    "Hello Kitty tem boca?",
+    "Não, Hello Kitty é normalmente desenhada sem boca"
+)
+
+criaCartao(
+    "Hello Kitty",
+    "Quantos personagens existem?",
+    "A Sanrio conta com mais de 400 personagens"
+)
+
+criaCartao(
+    "Hello Kitty",
+    "Cinnamoroll é um coelho?",
+    "Apesar da aparência, Cinnamoroll é um filhote de cachorro"
+)
+
+criaCartao(
+    "Hello Kitty",
+    "Quando é o aniversário do Badtz-maru?",
+    "O aniversário dele é em 1º de abril"
+)
+
+criaCartao(
+    "Hello Kitty",
+    "O que são os Little Twin Stars?",
+    "Os gêmeos Kiki e Lala são estrelas que possuem o tamanho da lua!"
+)
